8cd3d7cf4e5ec85c790d97e289d88af504c32470
Nicola <nicola.porta00@gmail.com>
2020-12-10 15:14:05 +0100

    Add condition

--- js/script.js
+++ js/script.js
@@ -15,15 +15,20 @@ var app = new Vue(
     },
     methods: {
       addNewElement: function() {
-        if(this.newElement != "")
-        this.list.push(this.newElement);
-        this.newElement = "";
+        if(this.newElement != "") {
+          if(this.list[0] = "BRAVO, HAI FATTO TUTTO!!!") {
+            this.list.splice(0,1);
+          }
+          this.list.push(this.newElement);
+          this.newElement = "";
+        }
       },
       remove: function (index){
         this.list.splice(index,1);
         if (this.list.length == 0) {
-          this.list[index] = "BRAVO, HAI FATTO TUTTO QUELLO CHE TI ERI PREFISSATO";
+          this.list[index] = "BRAVO, HAI FATTO TUTTO!!!";
           this.deleteState = "delete";
+          console.log(index);
         }
       }
     }
